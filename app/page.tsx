388ec91ef8b16fe66237dac9045230d771df9248
tanvir-bd <tanvirgreat@gmail.com>
2025-09-20 21:05:52 +0600

    deploy to alibaba vps1

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ export default async function Home() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
       <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
-        Superblog
+        Superblog  
       </h1>
       <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
         {users.map((user) => (
           <li key={user.id} className="mb-2">
             {user.name}
-            
+
           </li>
         ))}
       </ol>
