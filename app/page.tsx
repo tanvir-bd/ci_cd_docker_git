7baaffca8a5090cfb10edefb01e6d9e0d855ad08
tanvir-bd <tanvirgreat@gmail.com>
2025-09-27 09:50:36 +0600

    deploy to alibaba vps23

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import prisma from '@/lib/prisma'
 
+// Force dynamic rendering
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   const users = await prisma.user.findMany();
   return (
